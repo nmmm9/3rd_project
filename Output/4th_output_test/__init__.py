d0207cec48939db6ff4f8bc913aba6dcd39d5be3
nmmm9 <romanho23@naver.com>
2025-06-04 15:13:12 +0900

    진짜 찐최최종

---
+++ Output/4th_output_test/__init__.py
@@ -0,0 +1,9 @@
+"""
+3rd_project 테스트 패키지
+주요 컴포넌트별 테스트 케이스 포함:
+- API 엔드포인트 (test_app.py)
+- GitHub 저장소 분석 (test_github_analyzer.py)
+- 채팅 기능 (test_chat_handler.py)
+- 채팅 메모리 관리 (test_chat_memory.py)
+- 유틸리티 함수 (test_utils.py)
+""" 
